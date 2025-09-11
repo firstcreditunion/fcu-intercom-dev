e2ca4e098d8dabcd0c2a1433be245701808069e7
Isaac Vicliph <admin@axiomspace.co.nz>
2025-09-12 10:05:49 +1200

    Code-View Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import IntercomWrapper from '../components/IntercomWrapper'
 import Header from '@/components/Header'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Intercom | FCU Chatbot Dev',
@@ -21,6 +22,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(c,o,v,i,e,w){c[v]=c[v]||function(){(c[v].a=c[v].a||[]).push(arguments)};var s=o.createElement(i);s.src=e;s.async=1;var h=o.getElementsByTagName(i)[0];h.parentNode.insertBefore(s,h);c.addEventListener('error',function(err){c[v]('error',err)});c.addEventListener('message',function(msg){c[v]('message',msg)})})(window,document,'coview','script','https://cdn.coview.com/coview.js');
+coview('start', {
+  projectKey:'7r6Bv56bi6A'
+});
+            `,
+          }}
+        />
+      </head>
       <body className={`${poppins.className} antialiased`}>
         <Header />
         <IntercomWrapper>{children}</IntercomWrapper>
